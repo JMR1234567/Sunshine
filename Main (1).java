0fe32f519ecc65fb8860cf6d0f10212fb3dc1cfd
Casondra Simons <casondra.simons@smusd90.org>
2024-04-17 07:27:14 -0700

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,8 @@
+class Main{
+        public static void sun() {
+                System.out.println("shine");
+        }
+        public static void summer(int temp) {
+                
+        }
+}
